e8581eea293917a0f3d98eb8504f3d1201ea88ea
xieliang <xieliang555@gmail.com>
2020-03-15 13:23:05 +0800

    trained on csl

---
+++ Res3D/dataset.py
@@ -0,0 +1,82 @@
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+
+
+"""
+Implementation of Chinese Sign Language Dataset(50 signers with 5 times)
+"""
+class CSL_Isolated(Dataset):
+    def __init__(self, data_path, label_path, frames=16, num_classes=500, train=True, transform=None):
+        super(CSL_Isolated, self).__init__()
+        self.data_path = data_path
+        self.label_path = label_path
+        self.train = train
+        self.transform = transform
+        self.frames = frames
+        self.num_classes = num_classes
+        self.signers = 50
+        self.repetition = 5
+        if self.train:
+            self.videos_per_folder = int(0.8 * self.signers * self.repetition)
+        else:
+            self.videos_per_folder = int(0.2 * self.signers * self.repetition)
+        self.data_folder = []
+        try:
+            obs_path = [os.path.join(self.data_path, item) for item in os.listdir(self.data_path)]
+            self.data_folder = sorted([item for item in obs_path if os.path.isdir(item)])
+        except Exception as e:
+            print("Something wrong with your data path!!!")
+            raise
+        self.labels = {}
+        try:
+            label_file = open(self.label_path, 'r')
+            for line in label_file.readlines():
+                line = line.strip()
+                line = line.split('\t')
+                self.labels[line[0]] = line[1]
+        except Exception as e:
+            raise
+
+    def read_images(self, folder_path):
+        assert len(os.listdir(folder_path)) >= self.frames, "Too few images in your data folder: " + str(folder_path)
+        images = []
+        start = 1
+        step = int(len(os.listdir(folder_path))/self.frames)
+        for i in range(self.frames):
+            image = Image.open(os.path.join(folder_path, '{:06d}.jpg').format(start+i*step))  #.convert('L')
+            if self.transform is not None:
+                image = self.transform(image)
+            images.append(image)
+
+        images = torch.stack(images, dim=0)
+        # switch dimension for 3d cnn
+        images = images.permute(1, 0, 2, 3)
+        # print(images.shape)
+        return images
+
+    def __len__(self):
+        return self.num_classes * self.videos_per_folder
+
+    def __getitem__(self, idx):
+        top_folder = self.data_folder[int(idx/self.videos_per_folder)]
+        selected_folders = [os.path.join(top_folder, item) for item in os.listdir(top_folder)]
+        selected_folders = sorted([item for item in selected_folders if os.path.isdir(item)])
+        if self.train:
+            selected_folder = selected_folders[idx%self.videos_per_folder]
+        else:
+            selected_folder = selected_folders[idx%self.videos_per_folder + int(0.8*self.signers*self.repetition)]
+        images = self.read_images(selected_folder)
+        # print(selected_folder, int(idx/self.videos_per_folder))
+        # print(self.labels['{:06d}'.format(int(idx/self.videos_per_folder))])
+        # label = self.labels['{:06d}'.format(int(idx/self.videos_per_folder))]
+        label = torch.LongTensor([int(idx/self.videos_per_folder)])
+
+        return {'data': images, 'label': label}
+
+    def label_to_word(self, label):
+        if isinstance(label, torch.Tensor):
+            return self.labels['{:06d}'.format(label.item())]
+        elif isinstance(label, int):
+            return self.labels['{:06d}'.format(label)]
